41317611805f088af40fb03b9dd116676bd82c71
atto-jw <atto.jw@gmail.com>
2026-03-23 13:41:18 +0900

    add : FollowController 뼈대 작성

---
+++ src/main/java/com/ootd/fitme/domain/follow/controller/FollowController.java
@@ -0,0 +1,14 @@
+package com.ootd.fitme.domain.follow.controller;
+
+import com.ootd.fitme.domain.follow.service.FollowService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/follows")
+public class FollowController {
+
+    private final FollowService followService;
+}
