427116fb40ffc2c2ae8996fa6d9dc4e638eb4a21
atto-jw <atto.jw@gmail.com>
2026-03-24 09:32:52 +0900

    test : FollowTest 엔티티 단위 테스트 작성

---
+++ src/test/java/com/ootd/fitme/domain/follow/entity/FollowTest.java
@@ -0,0 +1,42 @@
+package com.ootd.fitme.domain.follow.entity;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class FollowTest {
+
+    @Test
+    @DisplayName("팔로우를 생성할 수 있다")
+    void create_follow_success() {
+
+        //given
+        UUID followerId = UUID.randomUUID();
+        UUID followeeId = UUID.randomUUID();
+
+        //when
+        Follow follow = Follow.create(followerId, followeeId);
+
+        //then
+        assertThat(follow.getFollowerId()).isEqualTo(followerId);
+        assertThat(follow.getFolloweeId()).isEqualTo(followeeId);
+    }
+
+    @Test
+    @DisplayName("자기 자신을 팔로우 할 수 없다")
+    void create_follow_fail() {
+
+        //given
+        UUID Id = UUID.randomUUID();
+
+        //when & then
+        assertThatThrownBy(() -> Follow.create(Id, Id))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자신을 팔로우 할 수 없습니다.");
+    }
+
+}
