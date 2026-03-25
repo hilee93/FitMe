aab6cb89a720fd69d4bbd10b3bb108326d01dc2d
atto-jw <atto.jw@gmail.com>
2026-03-25 11:48:30 +0900

    refactor: FollowDto를 FollowCursorDto로 대체 및 중복 코드 제거

--- src/main/java/com/ootd/fitme/domain/follow/repository/FollowRepositoryCustom.java
+++ src/main/java/com/ootd/fitme/domain/follow/repository/FollowRepositoryCustom.java
@@ -1,6 +1,7 @@
 package com.ootd.fitme.domain.follow.repository;
 
-import com.ootd.fitme.domain.follow.dto.response.FollowDto;
+import com.ootd.fitme.domain.follow.dto.response.FollowCursorDto;
+
 
 import java.util.List;
 import java.util.UUID;
@@ -8,8 +9,8 @@ import java.util.UUID;
 public interface FollowRepositoryCustom {
 
     // 팔로워 나를 팔로우 하는 사람 목록
-    List<FollowDto> findFollowers(UUID followeeId, String cursor, UUID idAfter, int limit, String nameLike);
+    List<FollowCursorDto> findFollowers(UUID followeeId, String cursor, UUID idAfter, int limit, String nameLike);
 
     // 팔로잉 내가 팔로우 하는 사람 목록
-    List<FollowDto> findFollowings(UUID followerId, String cursor, UUID idAfter, int limit, String nameLike);
+    List<FollowCursorDto> findFollowings(UUID followerId, String cursor, UUID idAfter, int limit, String nameLike);
 }
--- src/main/java/com/ootd/fitme/domain/follow/repository/FollowRepositoryImpl.java
+++ src/main/java/com/ootd/fitme/domain/follow/repository/FollowRepositoryImpl.java
@@ -1,6 +1,6 @@
 package com.ootd.fitme.domain.follow.repository;
 
-import com.ootd.fitme.domain.follow.dto.response.FollowDto;
+import com.ootd.fitme.domain.follow.dto.response.FollowCursorDto;
 import com.ootd.fitme.domain.follow.dto.response.UserSummary;
 import com.ootd.fitme.domain.follow.entity.QFollow;
 import com.ootd.fitme.domain.profile.entity.QProfile;
@@ -21,51 +21,40 @@ public class FollowRepositoryImpl implements FollowRepositoryCustom {
     private final JPAQueryFactory jpaQueryFactory;
 
     @Override
-    public List<FollowDto> findFollowers(UUID followeeId, String cursor, UUID idAfter, int limit, String nameLike) {
+    public List<FollowCursorDto> findFollowers(UUID followeeId, String cursor, UUID idAfter, int limit, String nameLike) {
 
         QFollow follow = QFollow.follow;
-        QProfile followeeProfile = new QProfile("followeeProfile");
         QProfile followerProfile = new QProfile("followerProfile");
 
-        return jpaQueryFactory
-                .select(
-                        Projections.constructor(FollowDto.class,
-                                follow.id,
-                                Projections.constructor(UserSummary.class,
-                                        follow.followeeId,
-                                        followeeProfile.name,
-                                        followeeProfile.profileImageUrl
-                                ),
-                                Projections.constructor(UserSummary.class,
-                                        follow.followerId,
-                                        followerProfile.name,
-                                        followerProfile.profileImageUrl
-                                )
-                        )
-                )
-                .from(follow)
-                .join(followeeProfile).on(followeeProfile.user.id.eq(follow.followeeId))
-                .join(followerProfile).on(followerProfile.user.id.eq(follow.followerId))
-                .where(
-                        follow.followeeId.eq(followeeId),
-                        nameLikeCondition(followerProfile, nameLike),
-                        cursorCondition(cursor, idAfter)
-                )
-                .orderBy(follow.createdAt.desc(), follow.id.asc())
-                .limit(limit + 1)
-                .fetch();
+        return findFollowList(
+                follow.followeeId.eq(followeeId),
+                followerProfile,
+                cursor, idAfter, limit, nameLike
+        );
     }
 
     @Override
-    public List<FollowDto> findFollowings(UUID followerId, String cursor, UUID idAfter, int limit, String nameLike) {
+    public List<FollowCursorDto> findFollowings(UUID followerId, String cursor, UUID idAfter, int limit, String nameLike) {
 
+        QFollow follow = QFollow.follow;
+        QProfile followeeProfile = new QProfile("followeeProfile");
+
+        return findFollowList(
+                follow.followerId.eq(followerId),
+                followeeProfile,
+                cursor, idAfter, limit, nameLike
+        );
+    }
+
+    private List<FollowCursorDto> findFollowList(
+            BooleanExpression condition, QProfile profile, String cursor, UUID idAfter, int limit, String nameLike) {
         QFollow follow = QFollow.follow;
         QProfile followeeProfile = new QProfile("followeeProfile");
         QProfile followerProfile = new QProfile("followerProfile");
 
         return jpaQueryFactory
                 .select(
-                        Projections.constructor(FollowDto.class,
+                        Projections.constructor(FollowCursorDto.class,
                                 follow.id,
                                 Projections.constructor(UserSummary.class,
                                         follow.followeeId,
@@ -76,15 +65,16 @@ public class FollowRepositoryImpl implements FollowRepositoryCustom {
                                         follow.followerId,
                                         followerProfile.name,
                                         followerProfile.profileImageUrl
-                                )
+                                ),
+                                follow.createdAt
                         )
                 )
                 .from(follow)
                 .join(followeeProfile).on(followeeProfile.user.id.eq(follow.followeeId))
                 .join(followerProfile).on(followerProfile.user.id.eq(follow.followerId))
                 .where(
-                        follow.followerId.eq(followerId),
-                        nameLikeCondition(followeeProfile, nameLike),
+                        condition,
+                        nameLikeCondition(profile, nameLike),
                         cursorCondition(cursor, idAfter)
                 )
                 .orderBy(follow.createdAt.desc(), follow.id.asc())
@@ -100,9 +90,10 @@ public class FollowRepositoryImpl implements FollowRepositoryCustom {
     private BooleanExpression cursorCondition(String cursor, UUID idAfter) {
         if (cursor == null) return null;
         Instant nextCursor = Instant.parse(cursor);
-        return QFollow.follow.createdAt.lt(nextCursor)
-                .or(QFollow.follow.createdAt.eq(nextCursor)
-                        .and(QFollow.follow.id.gt(idAfter)));
+        BooleanExpression condition = QFollow.follow.createdAt.lt(nextCursor);
+        if (idAfter != null) {
+            condition = condition.or(QFollow.follow.createdAt.eq(nextCursor).and(QFollow.follow.id.gt(idAfter)));
+        }
+        return condition;
     }
-
 }
