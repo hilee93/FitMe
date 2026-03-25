015c6109b00ab137d809acfd0f84f449605a5c40
hilee <eldh2002@gmail.com>
2026-03-25 16:50:37 +0900

    add: USER_NOT_FOUND error code to ErrorCode enum

--- src/main/java/com/ootd/fitme/domain/user/service/UserService.java
+++ src/main/java/com/ootd/fitme/domain/user/service/UserService.java
@@ -3,6 +3,7 @@ package com.ootd.fitme.domain.user.service;
 import com.ootd.fitme.domain.user.dto.request.SignInRequest;
 import com.ootd.fitme.domain.user.dto.request.UserCreateRequest;
 import com.ootd.fitme.domain.user.dto.response.JwtDto;
+import com.ootd.fitme.domain.user.dto.response.SignInResult;
 import com.ootd.fitme.domain.user.dto.response.UserDto;
 import com.ootd.fitme.domain.user.entity.User;
 
@@ -11,5 +12,7 @@ public interface UserService {
     boolean matchesPassword(String password, String encodedPassword);
     UserDto signUp(UserCreateRequest userCreateRequest);
     User validateSignIn(SignInRequest signInRequest);
-    JwtDto signIn(SignInRequest signInRequest);
+    SignInResult signIn(SignInRequest signInRequest);
+    JwtDto refresh(String refreshToken);
+    void signOut(String accessToken, String refreshToken);
 }
