f4bd04b41ab6b9bef2eea56c00dd81530ba36f16
atto-jw <atto.jw@gmail.com>
2026-03-26 14:50:40 +0900

    test: FollowControllerTest followeeId 파라미터 오타 수정

--- src/test/java/com/ootd/fitme/domain/follow/controller/FollowControllerTest.java
+++ src/test/java/com/ootd/fitme/domain/follow/controller/FollowControllerTest.java
@@ -218,7 +218,7 @@ class FollowControllerTest {
 
             // when & then
             mockMvc.perform(get("/api/follows/followers")
-                            .param("followeeId", followerId.toString())
+                            .param("followeeId", followeeId.toString())
                             .param("limit", "10"))
                     .andExpect(status().isOk())
                     .andExpect(jsonPath("$.data").isArray())
