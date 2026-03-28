409189e5410511026962be09d3ef87c819529cce
atto-jw <atto.jw@gmail.com>
2026-03-29 01:21:09 +0900

    feat: DirectMessageRepository queryDSL 목록조회 추가

--- src/main/java/com/ootd/fitme/domain/directmessage/repository/DirectMessageRepository.java
+++ src/main/java/com/ootd/fitme/domain/directmessage/repository/DirectMessageRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface DirectMessageRepository extends JpaRepository<DirectMessage, UUID> {
-
+public interface DirectMessageRepository extends JpaRepository<DirectMessage, UUID>, DirectMessageRepositoryCustom {
 
 }
