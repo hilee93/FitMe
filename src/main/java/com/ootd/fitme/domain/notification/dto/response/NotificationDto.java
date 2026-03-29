6a80727e7c77c174a410a05d89ae7f4163ff1e9e
sinjawon <qlqhroal@naver.com>
2026-03-30 01:41:11 +0900

    refactor: NotificationDto 필드명 변경 및 Enum 타입으로 level 수정

--- src/main/java/com/ootd/fitme/domain/notification/dto/response/NotificationDto.java
+++ src/main/java/com/ootd/fitme/domain/notification/dto/response/NotificationDto.java
@@ -1,15 +1,17 @@
 package com.ootd.fitme.domain.notification.dto.response;
 
+import com.ootd.fitme.domain.notification.enums.NotificationLevel;
+
 import java.time.Instant;
 import java.util.UUID;
 
 public record NotificationDto(
         UUID id,
         Instant createdAt,
-        UUID userId,
+        UUID receiverId,
         String title,
         String content,
-        String level
+        NotificationLevel level
 
 ) {
 }
