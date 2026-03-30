668c62588975e0e3d107c797df2b56424e5f9761
sinjawon <qlqhroal@naver.com>
2026-03-30 16:52:38 +0900

    feat: WeatherAlert 알림에 사용자 지역 기반 필터링 및 SSE 실시간 전송 추가

--- src/main/java/com/ootd/fitme/domain/notification/service/NotificationSseService.java
+++ src/main/java/com/ootd/fitme/domain/notification/service/NotificationSseService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
