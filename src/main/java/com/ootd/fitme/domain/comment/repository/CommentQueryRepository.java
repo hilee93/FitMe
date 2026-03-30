69e9d6f5aed0b28b35d25128a838bb22d61d2211
taeeonkim <myhom43@gmail.com>
2026-03-30 22:25:33 +0900

    feature: CommentQueryRepository목록 조회 쿼리문 작성, 테스트 코드 댓글 목록 조회 케이스 추가
    - 댓글 목록 조회 성공 시 반환 데이터 검증 테스트 추가
    - 댓글 수가 limit보다 적을 때 hasNext 값 검증 테스트 추가
    - CommentSearchCondition을 활용한 테스트 케이스 작성
    - 댓글 조회 관련 Nested 클래스 및 테스트 메서드 추가
    - 테스트 데이터 셋업 로직 수정 및 em.clear() 적용
    - 가독성을 위해 import 및 DisplayName 어노테이션 추가

--- src/main/java/com/ootd/fitme/domain/comment/repository/CommentQueryRepository.java
+++ src/main/java/com/ootd/fitme/domain/comment/repository/CommentQueryRepository.java
@@ -10,14 +10,19 @@ import com.ootd.fitme.domain.profile.entity.QProfile;
 import com.ootd.fitme.domain.user.entity.QUser;
 import com.ootd.fitme.global.exception.ErrorCode;
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
+import java.time.Instant;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 @RequiredArgsConstructor
+@Slf4j
 public class CommentQueryRepository {
 
     private final JPAQueryFactory queryFactory;
@@ -56,8 +61,70 @@ public class CommentQueryRepository {
     }
 
     public CursorResult<CommentResponseDto> findCommentsByFeedId(CommentSearchCondition condition) {
-        // TODO: 쿼리문 작성
-        return null;
+        QComment comment = QComment.comment;
+        QProfile profile = QProfile.profile;
+        QUser user = QUser.user;
+
+
+        int size = condition.limit();
+        log.debug("size: {}", size);
+
+        List<CommentFlatRow> commentFlatRows = queryFactory.select(
+                        Projections.constructor(
+                                CommentFlatRow.class,
+                                comment.id,
+                                comment.createdAt,
+                                comment.feed.id,
+                                comment.user.id,
+                                profile.name,
+                                profile.profileImageUrl,
+                                comment.content
+                        )
+                )
+                .from(comment)
+                .join(comment.user, user)
+                .join(profile).on(profile.user.eq(user))
+                .where(
+                        comment.feed.id.eq(condition.feedId()),
+                        cursorCondition(comment, condition.cursor(), condition.idAfter())
+                )
+                .orderBy(comment.createdAt.desc(), comment.id.asc())
+                .limit(size + 1)
+                .fetch();
+
+        boolean hasNext = commentFlatRows.size() > size;
+        if (hasNext) {
+            commentFlatRows.remove(size);
+        }
+
+        long total = countCommentsByFeedId(condition.feedId());
+
+        List<CommentResponseDto> commentResponseDtos = commentFlatRows.stream()
+                .map(CommentResponseDto::from)
+                .toList();
+        return new CursorResult<>(
+                commentResponseDtos,
+                hasNext,
+                total
+        );
+    }
+
+    private long countCommentsByFeedId(UUID feedId) {
+        QComment comment = QComment.comment;
+        Long total = queryFactory.select(comment.count())
+                .from(comment)
+                .where(comment.feed.id.eq(feedId))
+                .fetchOne();
+
+        return total == null ? 0 : total;
+    }
+
+    private BooleanExpression cursorCondition(QComment comment, String cursor, UUID idAfter) {
+        if (cursor == null || idAfter == null) return null;
+
+        Instant nextCursor = Instant.parse(cursor);
+        BooleanExpression condition = comment.createdAt.lt(nextCursor);
+        return condition.or(comment.createdAt.eq(nextCursor).and(comment.id.gt(idAfter)));
     }
 
 
--- src/test/java/com/ootd/fitme/domain/comment/repository/CommentQueryRepositoryTest.java
+++ src/test/java/com/ootd/fitme/domain/comment/repository/CommentQueryRepositoryTest.java
@@ -1,8 +1,12 @@
 package com.ootd.fitme.domain.comment.repository;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ootd.fitme.domain.comment.dto.request.CommentSearchCondition;
 import com.ootd.fitme.domain.comment.dto.response.CommentResponseDto;
 import com.ootd.fitme.domain.comment.entity.Comment;
 import com.ootd.fitme.domain.comment.exception.CommentNotFoundException;
+import com.ootd.fitme.domain.feed.dto.response.CursorResult;
 import com.ootd.fitme.domain.feed.entity.Feed;
 import com.ootd.fitme.domain.feed.fixture.FeedFixtureBuilder;
 import com.ootd.fitme.domain.feed.fixture.FeedFixtureBuilder.FeedFixture;
@@ -11,6 +15,7 @@ import com.ootd.fitme.domain.user.entity.User;
 import com.ootd.fitme.global.config.JpaAuditingConfig;
 import com.ootd.fitme.global.config.QuerydslConfig;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -23,7 +28,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DataJpaTest
-@Import({JpaAuditingConfig.class,QuerydslConfig.class, CommentQueryRepository.class, FeedFixtureBuilder.class})
+@Import({JpaAuditingConfig.class, QuerydslConfig.class, CommentQueryRepository.class, FeedFixtureBuilder.class})
 class CommentQueryRepositoryTest {
     @Autowired
     private CommentQueryRepository commentQueryRepository;
@@ -70,5 +75,66 @@ class CommentQueryRepositoryTest {
         // when & then
         assertThatThrownBy(() -> commentQueryRepository.findFeedCommentById(notExistingCommentId))
                 .isInstanceOf(CommentNotFoundException.class);
+    }
+
+    @Nested
+    @DisplayName("댓글 조회")
+    class FindFeedComments {
+
+        @Test
+        @DisplayName("댓글조회 - 피드 댓글 목록조회시limit 만큼 조회 및 hasNext true")
+        void findFeedComments_success_when_valid_request() throws JsonProcessingException {
+            // given
+            FeedFixture feedFixture = feedFixtureBuilder.createFeedFixture();
+            User user = feedFixture.user();
+            Feed feed = feedFixture.feed();
+
+            for (int i = 0; i < 30; i++) {
+                Comment comment = Comment.create("댓글 내용" + i, feed, user);
+                em.persist(comment);
+            }
+
+            em.flush();
+            em.clear();
+
+            CommentSearchCondition condition = new CommentSearchCondition(null, null, 20, feed.getId());
+
+            // when
+            CursorResult<CommentResponseDto> commentsByFeedId = commentQueryRepository.findCommentsByFeedId(condition);
+
+            // then
+            assertThat(commentsByFeedId.hasNext()).isTrue();
+            assertThat(commentsByFeedId.content().size()).isEqualTo(20);
+            assertThat(commentsByFeedId.total()).isEqualTo(30);
+            assertThat(commentsByFeedId.content().get(0).feedId()).isEqualTo(feed.getId());
+
+        }
+
+        @Test
+        @DisplayName("댓글조회 - 댓글 수가 limit보다 적으면 hasNext는 false")
+        void findFeedComments_hasNext_false_when_less_than_limit() {
+            // given
+            FeedFixture feedFixture = feedFixtureBuilder.createFeedFixture();
+            User user = feedFixture.user();
+            Feed feed = feedFixture.feed();
+
+            for (int i = 0; i < 10; i++) {
+                Comment comment = Comment.create("댓글 내용" + i, feed, user);
+                em.persist(comment);
+            }
+
+            em.flush();
+            em.clear();
+
+            CommentSearchCondition condition = new CommentSearchCondition(null, null, 20, feed.getId());
+
+            // when
+            CursorResult<CommentResponseDto> result = commentQueryRepository.findCommentsByFeedId(condition);
+
+            // then
+            assertThat(result.hasNext()).isFalse();
+            assertThat(result.content()).hasSize(10);
+            assertThat(result.total()).isEqualTo(10);
+        }
     }
 }
