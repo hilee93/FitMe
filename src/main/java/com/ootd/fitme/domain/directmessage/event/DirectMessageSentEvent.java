71a5ea4239b0c41544c1752a3c311f4a9eb59d9a
atto-jw <atto.jw@gmail.com>
2026-03-31 15:55:04 +0900

    remove: DirectMessageSentEvent 사용하지 않는 이벤트 제거

--- src/main/java/com/ootd/fitme/domain/directmessage/event/DirectMessageSentEvent.java
+++
@@ -1,8 +0,0 @@
-package com.ootd.fitme.domain.directmessage.event;
-
-import com.ootd.fitme.domain.directmessage.dto.response.DirectMessageDto;
-
-public record DirectMessageSentEvent(
-
-        DirectMessageDto directMessageDto
-) {}
