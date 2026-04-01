d3fe4640558f1e9488e5fa4060d51662439ab6fb
atto-jw <atto.jw@gmail.com>
2026-04-01 15:44:11 +0900

    feat: JwtChannelInterceptor WebSocket 적용 및 클라이언트 채널 인터셉터 설정

--- src/main/java/com/ootd/fitme/global/config/WebsocketConfig.java
+++ src/main/java/com/ootd/fitme/global/config/WebsocketConfig.java
@@ -1,6 +1,9 @@
 package com.ootd.fitme.global.config;
 
+import com.ootd.fitme.global.security.jwt.JwtChannelInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -8,7 +11,10 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final JwtChannelInterceptor jwtChannelInterceptor;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -22,5 +28,10 @@ public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/sub");
         registry.setApplicationDestinationPrefixes("/pub");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(jwtChannelInterceptor);
     }
 }
