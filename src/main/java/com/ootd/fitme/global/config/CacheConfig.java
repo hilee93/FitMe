f7f4581e006e899e1626da8ca6d969ef2189c7e7
JoSungYeon-000607 <zounsam00@gmail.com>
2026-04-13 13:25:14 +0900

    feature : cache관련 test 안정화 완료

--- src/main/java/com/ootd/fitme/global/config/CacheConfig.java
+++ src/main/java/com/ootd/fitme/global/config/CacheConfig.java
@@ -5,12 +5,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -26,18 +26,53 @@ import java.util.concurrent.TimeUnit;
 public class CacheConfig {
 
     @Bean
-    @Primary
-    public CacheManager redisCacheManager(RedisConnectionFactory factory, RedisCacheConfiguration config) {
+    @ConditionalOnProperty(name = "spring.cache.type", havingValue = "caffeine", matchIfMissing = true)
+    // 별도의 spring.cache.type 값이 없을 경우 자동으로 caffeine 생성하도록 세팅하였습니다
+    // (**별도 redis를 확인하고 싶으실 경우 꼭 프로퍼티 추가 부탁드립니다**)
+    public CacheManager caffeineCacheManager() {
+        log.info("[CacheConfig] Caffeine CacheManager 활성화");
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
 
+        cacheManager.setCaffeine(
+                Caffeine.newBuilder()
+                        .maximumSize(1000)
+                        .expireAfterWrite(10, TimeUnit.MINUTES)
+                        .recordStats()
+        );
+
+        cacheManager.registerCustomCache("attributes",
+                Caffeine.newBuilder()
+                        .maximumSize(500)
+                        .expireAfterWrite(10, TimeUnit.MINUTES)
+                        .recordStats()
+                        .build());
+
+        cacheManager.registerCustomCache("weatherforecast",
+                Caffeine.newBuilder()
+                        .maximumSize(500)
+                        .expireAfterWrite(30, TimeUnit.MINUTES)
+                        .recordStats()
+                        .build());
+
+        return cacheManager;
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "spring.cache.type", havingValue = "redis")
+    public CacheManager redisCacheManager(RedisConnectionFactory factory, RedisCacheConfiguration config) {
+        log.info("[CacheConfig] Redis CacheManager 활성화");
         return RedisCacheManager.builder(factory)
                 .cacheDefaults(config)
-                .withCacheConfiguration("weatherforecast", // NOTE: 이런식으로 자신 캐시이름, 세팅 아래 붙여서 추가
+                .withCacheConfiguration("attributes",
+                        config.entryTtl(Duration.ofMinutes(10)))
+                .withCacheConfiguration("weatherforecast",  // NOTE: 이런식으로 자신 캐시이름, 세팅 아래 붙여서 추가
                         config.entryTtl(Duration.ofMinutes(30)))
                 .enableStatistics()
                 .build();
     }
 
     @Bean
+    @ConditionalOnProperty(name = "spring.cache.type", havingValue = "redis")
     public RedisCacheConfiguration redisCacheConfiguration(ObjectMapper objectMapper) {
         ObjectMapper redisObjectMapper = objectMapper.copy();
         redisObjectMapper.activateDefaultTyping(
@@ -56,6 +91,4 @@ public class CacheConfig {
                 .entryTtl(Duration.ofSeconds(600))
                 .disableCachingNullValues();
     }
-
-
 }
